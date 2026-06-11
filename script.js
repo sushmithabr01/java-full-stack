a2819df7e75b6d5eb9e1538aba435abb3066a7ca
sushmithabr01 <sushmithabr80@gmail.com>
2026-06-11 14:11:19 +0530

    css layouts

---
+++ script.js
@@ -0,0 +1,57 @@
+// Simple elimination game: n people sit in a circle, eliminate every k-th person.
+
+/*let i=1;
+while(i<=30)
+{
+    console.log("Day  "+i+" completed");
+    i++;
+}*/
+/*let user_name="elsa";
+let password="chandu";
+let name="smitha";
+let pin="smily55";
+let name_1="pandu";
+let pin_1="pandu77";
+
+if(name=="smitha"){
+    console.log(user_name);
+    if(pin=="smily55")
+    {
+        console.log("loged in sucessfully");
+     }
+}*/
+
+/*for(let i=1;i<=10;i++)
+{
+    for(let j=1;j<=10;j++)
+    {
+    console.log("2 X "+i+" = "+(2*i));
+    }
+}*/
+/*let playGame(n,k) {
+   let player 
+  const eliminated = [];
+  let idx = 0;
+  while (people.length > 1) {
+    idx = (idx + k - 1) % people.length;
+    eliminated.push(people.splice(idx, 1)[0]);
+  }
+  console.log('Elimination order:', eliminated.join(' -> '));
+  console.log('Gift goes to person', people[0]);
+  return { eliminated, winner: people[0] };
+}
+playGame(5, 2);*/
+/*function cook()
+{
+    console.log("pour the dosa batter in tawa");
+    console.log("Rub it round order with garita");
+    console.log("pour some oil above dosa");
+    console.log("Twist it with dosa turner");
+    console.log("serve it to gnana and chandu");
+}
+for(i=0;i<4;i++){
+    cook();}*/
+    function dishwash(vessels){
+        console.log("We have to wash "+vessels+"vessels")
+    }
+    dishwash(2);
